23d19b5827e7fb6e436c606b13f677298d72253a
Kurganovich Maxim <rymiru@gmail.com>
2020-06-16 19:18:31 +0300

    Homework7Part12

--- 07_ConstraintLayoutAndToolbars/01_ConstraintLayout/Homework 7/app/src/main/java/com/skillbox/homework6/MainActivity.kt
+++ 07_ConstraintLayoutAndToolbars/01_ConstraintLayout/Homework 7/app/src/main/java/com/skillbox/homework6/MainActivity.kt
@@ -4,18 +4,14 @@ import android.os.Bundle
 import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.Gravity
 import android.view.View
 import android.widget.CompoundButton
-import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,12 +58,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        button.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                makeLoginOperation()
-            }
-
-        })
+        button.setOnClickListener { makeLoginOperation() }
     }
 
 //    Метод для обработчика кнопки. При нажатии на кнопку все элементы становятся недоступными,
@@ -88,7 +79,7 @@ class MainActivity : AppCompatActivity() {
         ConstraintSet().apply {
             constrainHeight(progressBar.id, ConstraintSet.WRAP_CONTENT)
             constrainWidth(progressBar.id, ConstraintSet.WRAP_CONTENT)
-            connect(progressBar.id, ConstraintSet.TOP, R.id.button, ConstraintSet.BOTTOM)
+            connect(progressBar.id, ConstraintSet.TOP, R.id.editPassword, ConstraintSet.BOTTOM)
             connect(progressBar.id, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT)
             connect(progressBar.id, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT)
         }.applyTo(container)
