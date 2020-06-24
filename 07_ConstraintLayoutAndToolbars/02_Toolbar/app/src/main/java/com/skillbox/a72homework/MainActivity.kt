b4b4d0873e0b56c196b4e8ca976106f692153c06
Kurganovich Maxim <rymiru@gmail.com>
2020-06-24 16:33:26 +0300

    Homework7.2

--- 07_ConstraintLayoutAndToolbars/02_Toolbar/app/src/main/java/com/skillbox/a72homework/MainActivity.kt
+++ 07_ConstraintLayoutAndToolbars/02_Toolbar/app/src/main/java/com/skillbox/a72homework/MainActivity.kt
@@ -4,17 +4,10 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val users = listOf(
-        "User1",
-        "User2",
-        "User3",
-        "User4",
-        "User5",
-        "Unknown"
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createToolbar() {
+        // Листнер, который обрабатывает нажатие на иконки и меню в тулбаре
         toolbar.setOnMenuItemClickListener { item: MenuItem ->
             when (item.itemId) {
                 R.id.action_1_menu -> {
@@ -43,10 +37,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        //Листнер, который обрабатывает нажатие на навигационную кнопку (кнопка "Назад") в тулбаре
         toolbar.setNavigationOnClickListener {
             toast("Back")
         }
 
+        //Листнер, который обрабатывает раскрытие и закрытие поиска
         val search = toolbar.menu.findItem(R.id.menu_search)
         search.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
             override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
@@ -56,29 +52,36 @@ class MainActivity : AppCompatActivity() {
 
             override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
                 expandTextView.text = "Search Collapsed"
+                searchTextView.text = ""
                 return true
             }
         })
 
-        (search.actionView as androidx.appcompat.widget.SearchView).setOnQueryTextListener(object :
-            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+        //Листнер, который обрабатывает ввод текста. Первый метод отвечает за обработку при нажатии на клавишу поиск
+        //Второй метод отвечает за обработку при вводе текста в поисковик
+        (search.actionView as SearchView).setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return true
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                users.filter { it.contains(newText ?: "", ignoreCase = true) }
-                    .joinToString()
-                    .let {
-                        searchTextView.text = it
+                if (newText != null) {
+                    if (newText.isNotEmpty()) {
+                        var searchText = Regex("""\s(\w*$newText\w*)\s""").findAll(longTextView.text)
+                        val result = StringBuilder()
+                        for (item in searchText) {
+                            result.append(item.value + "")
+                        }
+                        searchTextView.text = result
                     }
+                }
                 return true
             }
-
         })
-
     }
 
+    //Функция, которая вызывает Тоасты
     private fun toast(text: String) {
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
